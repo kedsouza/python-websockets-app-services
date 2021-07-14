4e6caddd15e89a58b4d03a9e58ba1ceeb7bb70bf
Keegan D'Souza <kedsouza@microsoft.com>
2021-07-14 00:05:13 -0500

    removed files

--- server.py
+++ server.py
@@ -2,28 +2,28 @@ import asyncio
 import websockets
 import os
 import logging
+import time
 
+connected = set()
 
 port = os.environ['PORT']
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
 
-print('Here')
-print(port)
+async def producer():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time + " Number or connected clients: " + str(len(connected))
 
+async def hello(websocket, path):
+    try:
+        while True:
+            connected.add(websocket)
+            message = await producer()
+            await websocket.send(message)
+            await asyncio.sleep(1)
+    finally:
+        connected.remove(websocket)
 
-async def hello(websockets, path):
-    name = await websockets.recv()
-    print(f"< {name}")
-
-    greeting = f"Hello {name}!"
-
-    await websockets.send(greeting)
-    print(f"> {greeting}")
-
-start_server = websockets.serve(hello, "0.0.0.0", port )
+start_server = websockets.serve(hello, "0.0.0.0", port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-    
